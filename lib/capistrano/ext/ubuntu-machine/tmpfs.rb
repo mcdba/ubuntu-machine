163067042cccbbe542ed41c6ce629a1721d6f8b3
Filip H.F. "FiXato" Slagter <fixato@gmail.com>
2009-06-18 14:58:51 +0200

    tmpfs:create_vsftpd_tmpfs_dirs adds the needed commands to crontab onstart as well.

--- lib/capistrano/ext/ubuntu-machine/tmpfs.rb
+++ lib/capistrano/ext/ubuntu-machine/tmpfs.rb
@@ -17,10 +17,20 @@ namespace :tmpfs do
 
   desc "Create ftp directories within :vsftpd_tmpfs_directory for each user defined in :vsftpd_users and symlink in their homedirs"
   task :create_vsftpd_tmpfs_dirs, :roles => :app do
+    cron_commands = []
     vsftpd_users.each do |target_user|
-      sudo "mkdir -p #{File.join(vsftpd_tmpfs_directory,target_user)}"
+      target_tmpfs_dir = File.join(vsftpd_tmpfs_directory,target_user)
+      cmd = "mkdir -p #{target_tmpfs_dir} && chown #{target_user}:#{vsftpd_group} #{target_tmpfs_dir} && chmod 0777 #{target_tmpfs_dir}"
+      sudo cmd
+      cron_commands << cmd
       sudo "ln -s #{File.join(vsftpd_tmpfs_directory,target_user)} ~#{target_user}/ftp"
-      sudo "chown #{target_user}:#{vsftpd_group} ~#{target_user}/ftp"
     end
+
+    tmp_cron="/tmp/tmp_cron"
+    sudo "crontab -l > #{tmp_cron}"
+    cron_commands.each do |cmd|
+      run "echo '@reboot #{cmd} > /dev/null 2>&1' > #{tmp_cron}"
+    end
+    sudo "crontab #{tmp_cron}"
   end
 end
