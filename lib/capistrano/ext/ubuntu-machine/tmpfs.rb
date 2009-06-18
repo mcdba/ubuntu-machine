0ceb8289fe2315059f93581cb14cb359b354489f
Filip H.F. "FiXato" Slagter <fixato@gmail.com>
2009-06-18 14:09:55 +0200

    Added tmpfs:create_vsftpd_tmpfs_dirs. It is quick solution for a project though; a better solution will be worked on later.

--- lib/capistrano/ext/ubuntu-machine/tmpfs.rb
+++ lib/capistrano/ext/ubuntu-machine/tmpfs.rb
@@ -14,4 +14,13 @@ namespace :tmpfs do
       sudo "mount #{dir}"
     end
   end
+
+  desc "Create ftp directories within :vsftpd_tmpfs_directory for each user defined in :vsftpd_users and symlink in their homedirs"
+  task :create_vsftpd_tmpfs_dirs, :roles => :app do
+    vsftpd_users.each do |target_user|
+      sudo "mkdir -p #{File.join(vsftpd_tmpfs_directory,target_user)}"
+      sudo "ln -s #{File.join(vsftpd_tmpfs_directory,target_user)} ~#{target_user}/ftp"
+      sudo "chown #{target_user}:#{vsftpd_group} ~#{target_user}/ftp"
+    end
+  end
 end
