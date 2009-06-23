a922f5523fa35fd5a96d5629a2b5f81e8b63ada0
Filip H.F. "FiXato" Slagter <fixato@gmail.com>
2009-06-23 22:01:28 +0200

    Added ODBC support based on U-Ezekiel\Tarik Alkasab's "talkasab/master" fork of Ubuntu Machine.
    Some changes include:
     - Restructured the odbc configuration templates a bit to match a client's config.
     - Added _cset for the required variables.
     - Not adding the mod_deflate patches that were included in one of Alkasab's ODBC patches.
     - Requires are added in alphabetical order.
    
    Based on the following patches:
     - http://github.com/talkasab/ubuntu-machine/commit/6db81e805478ba986aec676b1f5913bc862399c5
     - http://github.com/talkasab/ubuntu-machine/commit/92d2e1da4f3562441673a4c62d9fbeab9f68c0ba
     - http://github.com/talkasab/ubuntu-machine/commit/d2282b20f0ccb69a4c6d30d93845b84dcf9a0fa8
    
    Note to Tarik Alkasab: Thanks!
    
    Squashed commit of the following:
    
    commit 1d7b6863fc91111c12d26c18ff53ab47eab2e3c2
    Author: Filip H.F. "FiXato" Slagter <fixato@gmail.com>
    Date:   Tue Jun 23 20:46:44 2009 +0200
    
        Require variables for odbc to be set.
    
    commit ad1ad538d717ed54b8d0e55e1c8641dd93c903a8
    Author: Filip H.F. "FiXato" Slagter <fixato@gmail.com>
    Date:   Wed Jun 17 18:04:43 2009 +0200
    
        Changed odbc/freetds config templates to match a setup we used for a client.
    
    commit deea2d137fccaaed8e52040def13fc37e0097c73
    Author: Filip H.F. "FiXato" Slagter <fixato@gmail.com>
    Date:   Tue Jun 23 19:56:33 2009 +0200
    
        Added ODBC configuration files.
        Conflicts:
    
        	ubuntu-machine.gemspec
    
    commit ee46071cf1229de6970c7fb1c5d2cf2a1c35aa03
    Author: U-Ezekiel\Tarik Alkasab <Tarik Alkasab@Ezekiel.(none)>
    Date:   Sun Apr 5 03:47:03 2009 -0400
    
        Added the ruby-odbc library.
    
    commit c4c2f45be396abbf7915b25f8252b7bf0c804813
    Author: Filip H.F. "FiXato" Slagter <fixato@gmail.com>
    Date:   Tue Jun 23 19:49:48 2009 +0200
    
        Added bringing in the ODBC and FreeTDS libraries.
    
        Conflicts:
    
        	lib/capistrano/ext/ubuntu-machine.rb
        	ubuntu-machine.gemspec

---
+++ lib/capistrano/ext/ubuntu-machine/odbc.rb
@@ -0,0 +1,51 @@
+namespace :odbc do
+  _cset :odbc_sourcename  { abort "Please specify the odbc sourcename:\n  set :odbc_sourcename, 'MyFirstSQLServer'" }
+  _cset :odbc_database    { abort "Please specify the odbc database:\n  set :odbc_database, 'MyDB'" }
+  _cset :odbc_host        { abort "Please specify the odbc host:\n  set :odbc_host, '127.0.0.1'" }
+  _cset :odbc_port,       '1433'
+
+  desc "Install ODBC/FreeTDS"
+  task :install, :roles => :app do
+    run "cp /etc/profile ~/profile.tmp"
+    run "echo 'export ODBCINI=/etc/odbc.ini' >> ~/profile.tmp"
+    run "echo 'export ODBCSYSINI=/etc' >> ~/profile.tmp"
+    run "echo 'export FREETDSCONF=/etc/freetds/freetds.conf' >> ~/profile.tmp"
+    sudo "mv ~/profile.tmp /etc/profile"
+
+    freetds = "freetds-0.82"
+    sudo "sudo apt-get install unixodbc unixodbc-dev tdsodbc -y"
+    run "wget -nv ftp://ftp.ibiblio.org/pub/Linux/ALPHA/freetds/stable/#{freetds}.tar.gz"
+    run "tar xvzf #{freetds}.tar.gz"
+    run "cd #{freetds}"
+    run "cd #{freetds} && ./configure"
+    run "cd #{freetds} && make"
+    run "cd #{freetds} && sudo make install"
+    run "rm #{freetds}.tar.gz"
+    run "rm -Rf #{freetds}"
+  end
+
+  desc "Install the ruby ODBC library"
+  task :install_rubyodbc, :roles => :app do
+    rubyodbc = "ruby-odbc-0.9996"
+    run "wget -nv http://www.ch-werner.de/rubyodbc/#{rubyodbc}.tar.gz"
+    run "tar xvzf #{rubyodbc}.tar.gz"
+    run "cd #{rubyodbc}"
+    run "cd #{rubyodbc} && ruby extconf.rb"
+    run "cd #{rubyodbc} && make"
+    run "cd #{rubyodbc} && sudo make install"
+    run "rm #{rubyodbc}.tar.gz"
+    run "rm -Rf #{rubyodbc}"
+  end
+
+  desc "Install FreeTDS/ODBC configuration files"
+  task :config_files, :roles => :app do
+    put render("odbc.ini", binding), "odbc.ini"
+    sudo "mv odbc.ini /etc/odbc.ini"
+    put render("odbcinst.ini", binding), "odbcinst.ini"
+    sudo "mv odbcinst.ini /etc/odbcinst.ini"
+    put render("freetds.conf", binding), "more_freetds.conf"
+    run "cat /etc/freetds/freetds.conf more_freetds.conf > freetds.conf"
+    sudo "mv freetds.conf /etc/freetds/freetds.conf"
+    run "rm more_freetds.conf"
+  end
+end
