ddfbbb5978694f25ee1d4d66e6d76b33267e28ca
Filip H.F. "FiXato" Slagter <fixato@gmail.com>
2009-09-20 20:15:21 +0200

    Bugfix: Added the missing namespace for the extras.

--- lib/capistrano/ext/ubuntu-machine/extras.rb
+++ lib/capistrano/ext/ubuntu-machine/extras.rb
@@ -1,33 +1,35 @@
-desc "Installs extra utils: curl, lynx, mailutils, munin, imagemagick"
-task :install_all do
-  install_curl
-  install_lynx
-  install_mailutils
-  install_munin
-  install_imagemagick
-end
+namespace :extras do
+  desc "Installs extra utils: curl, lynx, mailutils, munin, imagemagick"
+  task :install_all do
+    install_curl
+    install_lynx
+    install_mailutils
+    install_munin
+    install_imagemagick
+  end
 
-desc "Installs extra util curl"
-task :install_curl do
-  sudo "aptitude install -y curl"
-end
+  desc "Installs extra util curl"
+  task :install_curl do
+    sudo "aptitude install -y curl"
+  end
 
-desc "Installs extra util lynx"
-task :install_lynx do
-  sudo "aptitude install -y lynx"
-end
+  desc "Installs extra util lynx"
+  task :install_lynx do
+    sudo "aptitude install -y lynx"
+  end
 
-desc "Installs extra util mailutils"
-task :install_mailutils do
-  sudo "aptitude install -y mailutils"
-end
+  desc "Installs extra util mailutils"
+  task :install_mailutils do
+    sudo "aptitude install -y mailutils"
+  end
 
-desc "Installs extra util munin"
-task :install_munin do
-  sudo "aptitude install -y munin"
-end
+  desc "Installs extra util munin"
+  task :install_munin do
+    sudo "aptitude install -y munin"
+  end
 
-desc "Installs extra util imagemagick"
-task :install_imagemagick do
-  sudo "aptitude install -y imagemagick"
+  desc "Installs extra util imagemagick"
+  task :install_imagemagick do
+    sudo "aptitude install -y imagemagick"
+  end
 end
