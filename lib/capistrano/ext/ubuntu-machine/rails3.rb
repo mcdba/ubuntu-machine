3033b32c3f8b65e789629b107be4d2f6999eeae4
Rachid Al Maach <rachid.al.maach@gmail.com>
2010-07-27 20:26:36 +0200

    added additional imagemagick installation commands and rmagick

--- lib/capistrano/ext/ubuntu-machine/rails3.rb
+++ lib/capistrano/ext/ubuntu-machine/rails3.rb
@@ -2,5 +2,6 @@ namespace :rails3 do
   desc "Install Rails3"
   task :install, :roles => :app do
      sudo "/opt/ruby-enterprise/bin/gem install rails --pre --no-ri --no-rdoc"
+     sudo "apt-get install libxml2-dev libxslt1-dev"
     end
 end
