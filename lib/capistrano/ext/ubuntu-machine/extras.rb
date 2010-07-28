84bac20691dc5e3739aae18fa81ca954fe58009b
Rachid Al Maach <rachid.al.maach@gmail.com>
2010-07-28 09:36:38 +0200

    merged stuff from original ubuntu-machine

---
+++ lib/capistrano/ext/ubuntu-machine/extras.rb
@@ -0,0 +1,39 @@
+namespace :extras do
+  desc "Installs extra utils: curl, lynx, mailutils, munin, imagemagick"
+  task :install_all do
+    install_curl
+    install_lynx
+    install_mailutils
+    install_munin
+    install_imagemagick
+  end
+
+  desc "Installs extra util curl"
+  task :install_curl do
+    sudo "aptitude install -y curl"
+  end
+
+  desc "Installs extra util lynx"
+  task :install_lynx do
+    sudo "aptitude install -y lynx"
+  end
+
+  desc "Installs extra util mailutils"
+  task :install_mailutils do
+    sudo "aptitude install -y mailutils"
+  end
+
+  desc "Installs extra util munin"
+  task :install_munin do
+    sudo "aptitude install -y munin"
+  end
+
+  desc "Installs extra util imagemagick"
+  task :install_imagemagick do
+    sudo "apt-get install libmagick9-dev librmagick-ruby1.8 librmagick-ruby libmagickcore-dev msttcorefonts imagemagick libmagickwand-dev"
+    sudo "ldconfig"
+    sudo "gem install rmagick"
+  end
+end
+
+#
---
+++ lib/capistrano/ext/ubuntu-machine/tmpfs.rb
@@ -0,0 +1,17 @@
+namespace :tmpfs do
+  _cset(:tmpfs_directories) do
+    abort "Please specify the tmpfs directories:\n  set :tmpfs_directories do\n{\n'/tmpfs' => {:size => '2G', :mode => '0744'},\n}\nend"
+  end
+
+  desc "Create tmpfs directories"
+  task :create_directories, :roles => :app do
+    tmpfs_directories.each do |dir,options|
+      options[:size] ||= '2G'
+      options[:mode] ||= '0744'
+      sudo "mkdir -p #{dir}"
+      fstab_line = "tmpfs #{dir} tmpfs size=#{options[:size]},mode=#{options[:mode]} 0 0"
+      sudo_add_to_file('/etc/fstab',fstab_line)
+      sudo "mount #{dir}"
+    end
+  end
+end
