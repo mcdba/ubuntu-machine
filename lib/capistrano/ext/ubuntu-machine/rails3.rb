75bceb124c78d9f48b7ccbe462e868375c1d463f
Rachid Al Maach <rachid.al.maach@gmail.com>
2010-07-28 10:36:06 +0200

    removed chown line

--- lib/capistrano/ext/ubuntu-machine/rails3.rb
+++ lib/capistrano/ext/ubuntu-machine/rails3.rb
@@ -3,6 +3,5 @@ namespace :rails3 do
   task :install, :roles => :app do
      sudo "/opt/ruby-enterprise/bin/gem install rails --pre --no-ri --no-rdoc"
      sudo "apt-get install libxml2-dev libxslt1-dev"
-     sudo "chown #{user_to_create}:#{user_to_create} /home/#{user_to_create}/.gem"
     end
 end
